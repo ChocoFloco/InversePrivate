dd8f87701e19b7ccfab2b84e89d001446a9c7440
ChocoFloco <149216961+ChocoFloco@users.noreply.github.com>
2023-10-31 11:35:55 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,17 @@
 // Initialize Firebase
+
+
+ const firebaseConfig = {
+    apiKey: "AIzaSyC7mybMry1LzLSlpsmXMlN_9LbcPogoW2g",
+    authDomain: "inverseprivate-304e0.firebaseapp.com",
+    projectId: "inverseprivate-304e0",
+    storageBucket: "inverseprivate-304e0.appspot.com",
+    messagingSenderId: "246587992179",
+    appId: "1:246587992179:web:a5a5fc429c9e9c7d2ca206",
+    measurementId: "G-TGV55BWQJW"
+  };
+
+
 // TODO: Missing Firebase Config
 firebase.initializeApp(config);
 this.firebaseToken = document.querySelector("#firebase-token");
